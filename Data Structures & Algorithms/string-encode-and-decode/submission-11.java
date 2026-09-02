234a02ef737e806e16142596474e311cc0dc329c
Tony Simav Fernandes <96526835+Tsfdes@users.noreply.github.com>
2026-09-02 20:06:00 +0530

    Add: string-encode-and-decode - submission-11

---
+++ Data Structures & Algorithms/string-encode-and-decode/submission-11.java
@@ -0,0 +1,49 @@
+class Solution {
+    public String encode(List<String> strs) {
+        StringBuilder encoded = new StringBuilder("");
+        for (String s : strs) {
+            encoded.append("*#").append(s.length()).append("*#").append(s);
+        }
+        return encoded.toString();
+    }
+
+    public List<String> decode(String str) {
+        List<String> list = new ArrayList<>();
+
+        for (int i = 0; i < str.length(); i++) {
+            if (str.charAt(i) == '*' && i + 1 < str.length() && str.charAt(i + 1) == '#') {
+
+                StringBuilder count = new StringBuilder("");
+                i += 2;
+                while (str.charAt(i) != '*') {
+                    count.append( str.charAt(i) );
+                    i++;
+                }
+
+                i += 2;
+                int cnt = Integer.parseInt(count.toString());
+                StringBuilder st = new StringBuilder("");
+
+                while (cnt > 0) {
+                    st.append(str.charAt(i));
+                    i++;
+                    cnt--;
+                }
+
+                list.add(st.toString());
+                i--;
+
+            } else {
+              
+                StringBuilder st = new StringBuilder("");
+                while (i < str.length()) {
+                    st.append(str.charAt(i));
+                    i++;
+                }
+                list.add(st.toString());
+            }
+        }
+
+        return list;
+    }
+}
